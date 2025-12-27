d14970b6d9c8d7628ffd070b745d82c4728f1e96
Muhammed Nasser <mo.nasser20032003@gmail.com>
2025-12-27 19:46:49 +0200

    add landing page structure and exam functionality

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,2 +1,30 @@
-import { questions } from "../data/questions.js";
-console.log(questions);
+const checkBtn = document.querySelector('input[type="checkbox"]');
+const button = document.getElementById("startExam");
+const warningMsg = document.getElementById("warningMsg");
+
+// button.classList.add("bg-[#9e9d9d]");
+// button.classList.add("cursor-not-allowed");
+checkBtn.addEventListener("change", () => {
+  if (checkBtn.checked) {
+    button.disabled = false;
+    button.classList.remove("bg-[#9e9d9d]", "cursor-not-allowed");
+    button.classList.add("bg-[#09c]", "cursor-allowed");
+
+    return;
+  }
+  button.classList.remove("bg-[#09c]", "cursor-allowed");
+  button.classList.add("bg-[#9e9d9d]", "cursor-not-allowed");
+
+  return;
+});
+
+button.addEventListener("click", () => {
+  if (!checkBtn.checked) {
+    warningMsg.classList.remove("hidden");
+    return;
+  }
+
+  sessionStorage.setItem("examStarted", "true");
+  window.location.replace("./exam.html");
+  //   window.location.href = "./exam.html";
+});
