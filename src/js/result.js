6788d21457c68aa12c15ad89b6a5120080ee0470
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-01-02 23:58:57 +0200

    shock message

--- src/js/result.js
+++ src/js/result.js
@@ -15,6 +15,7 @@ var percentage = (score / questions.length) * 100;
 var circle = document.querySelector(".progress-circle");
 var text = circle.querySelector("span");
 var message = document.querySelector(".score-message");
+var shockMessage = document.querySelector(".shock-message");
 
 circle.style.setProperty("--value", percentage);
 text.textContent = percentage + "%";
@@ -31,6 +32,11 @@ if (percentage >= 60) {
     Congratulations! You passed the exam with
     <strong>${percentage}%</strong>. Keep up the good work!
   `;
+  shockMessage.innerHTML = `
+    <img src="./assets/icons/icons8-correct-30.png" alt="" class="w-[16px] h-[16px]"> Congratulations
+  `;
+
+  shockMessage.classList.add("border-green-600", "text-green-600", "bg-green-600/10");
 } else {
   console.log("failed");
 
@@ -43,6 +49,19 @@ if (percentage >= 60) {
     Unfortunately, you did not meet the passing criteria of
     <strong>60%</strong>. Don't be discouraged, review your weak areas and try again.
   `;
+
+  shockMessage.innerHTML = `
+  <span
+                class="flex items-center text-sm justify-center w-[16px] h-[16px] text-center bg-red-600 text-white rounded-full"
+                >!</span
+              >Faild
+  `;
+    shockMessage.classList.add(
+      "border-red-600",
+      "text-red-600",
+      "bg-red-600/10"
+    );
+
 }
 
 // performance summary
